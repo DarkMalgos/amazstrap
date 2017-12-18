052d362e18833397ae03e12757191a5858a3b134
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-12-18 10:51:02 +0100

    header & index v1.1

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Amazone</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</head>
+<body>
+    <main class="container-fluid p-0">
+        <?php
+            include('views/header.html');
+        ?>
+        <section style="height:calc(100vh - 104px);">
+            <div id="carouselExampleIndicators" class="carousel slide h-100" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner h-100">
+                    <div class="carousel-item active h-100">
+                        <article class="d-block w-100 h-100 bg-home1">
+                            <div class="row align-items-center h-100">
+                                <div class="col-md-6 h-100"></div>
+                                <div class="col-md-6">
+                                    <h2 class="carousel-title">Un Noël exceptionnel</h2>
+                                    <p class="carousel-btext">Des milliers d'offres jusqu'a -60%</p>
+                                    <p class="carousel-text">Payez en 4 fois sans frais pour les offres éligibles.*</p>
+                                    <a class="carousel-condition">*voir conditions</a>
+                                    <button type="button" class="btn btn-primary">Le noël Amazon</button>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="carousel-item h-100">
+                        <article class="d-block w-100 h-100 bg-home1">
+                            <div class="row align-items-center h-100">
+                                <div class="col-md-6 h-100"></div>
+                                <div class="col-md-6">
+                                    <h2 class="carousel-title">Un Noël exceptionnel</h2>
+                                    <p class="carousel-btext">Des milliers d'offres jusqu'a -60%</p>
+                                    <p class="carousel-text">Payez en 4 fois sans frais pour les offres éligibles.*</p>
+                                    <a class="carousel-condition">*voir conditions</a>
+                                    <button type="button" class="btn btn-primary">Le noël Amazon</button>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="carousel-item h-100">
+                        <article class="d-block w-100 h-100 bg-home1">
+                            <div class="row align-items-center h-100">
+                                <div class="col-md-6 h-100"></div>
+                                <div class="col-md-6">
+                                    <h2 class="carousel-title">Un Noël exceptionnel</h2>
+                                    <p class="carousel-btext">Des milliers d'offres jusqu'a -60%</p>
+                                    <p class="carousel-text">Payez en 4 fois sans frais pour les offres éligibles.*</p>
+                                    <a class="carousel-condition">*voir conditions</a>
+                                    <button type="button" class="btn btn-primary">Le noël Amazon</button>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="mx-2 mt-3">
+            <h3>VENTES FLASH</h3>
+            <div class="row justify-content-between w-100">
+                <article class="card col-md-6">
+                    <img class="card-img-top" src="assets/img/" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </article>
+                <article class="card col-md-6" style="width: 20rem;">
+                    <img class="card-img-top" src="assets/img/" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </article>
+            </div>
+            <div class="row mt-3 justify-content-between w-100">
+                <article class="card col-md-4">
+                    <img class="card-img-top" src="assets/img/" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </article>
+                <article class="card col-md-5" style="width: 20rem;">
+                    <img class="card-img-top" src="assets/img/" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </article>
+                <article class="card col-md-3" style="width: 20rem;">
+                    <img class="card-img-top" src="assets/img/" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
