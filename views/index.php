5d0927cf3b9c11ad38e5dec8db665a1e18300566
Florian SAUMUROT <florian.saumurot@gmail.com>
2018-01-08 10:28:03 +0100

    correction orthographe

--- views/index.php
+++ views/index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Amazone</title>
+    <title>Amazon</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
